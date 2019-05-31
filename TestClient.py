b01a43d3287b93b6b0d4a9a3fe011bb06eeb139e
Dawand Sulaiman <djs21@st-andrews.ac.uk>
2019-05-31 01:51:18 +0100

    ignoring APK and java_classes directories

--- TestClient.py
+++ TestClient.py
@@ -48,98 +48,93 @@ from os import environ
 from autobahn.asyncio.component import Component, run
 
 rpcname = "uk.ac.standrews.cs.mamoc.SearchText.KMP"
-code = """
-package uk.ac.standrews.cs.mamoc.SearchText;
+class_code = """
+package uk.ac.standrews.cs.mamoc_demo.SearchText;
 
 import uk.ac.standrews.cs.mamoc_client.Annotation.Offloadable;
 
 @Offloadable(resourceDependent = true, parallelizable = true)
 public class KMP {
-    String content;
-    String pattern;
-    
-    public KMP(String content, String pat) {
+
+    String content, pattern;
+
+    public KMP(String content, String pattern) {
         this.content = content;
-        this.pattern = pat;
+        this.pattern = pattern;
     }
 
     public int run() {
         int matches = 0;
         int M = pattern.length();
         int N = content.length();
-        
+
         // create lps[] that will hold the longest
         // prefix suffix values for pattern
         int lps[] = new int[M];
         int j = 0; // index for pat[]
-        
+
         // Preprocess the pattern (calculate lps[] array)
         computeLPSArray(pattern, M, lps);
-        
+
         int i = 0; // index for txt[]
-        
         while (i < N) {
             if (pattern.charAt(j) == content.charAt(i)) {
                 j++;
                 i++;
             }
-            
             if (j == M) {
                 matches++;
                 j = lps[j - 1];
             }
-            
+
             // mismatch after j matches
-            
             else if (i < N && pattern.charAt(j) != content.charAt(i)) {
-            
                 // Do not match lps[0..lps[j-1]] characters,
                 // they will match anyway
-                
                 if (j != 0)
-                    j = lps[j - 1];    
+                    j = lps[j - 1];
                 else
                     i = i + 1;
             }
         }
-        
+
         return matches;
     }
 
-               private void computeLPSArray(String pat, int M, int lps[])
-               {
-                   // length of the previous longest prefix suffix
-                   int len = 0;
-                   int i = 1;
-                   lps[0] = 0; // lps[0] is always 0
+    private void computeLPSArray(String pat, int M, int lps[])
+    {
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
 
-                   // the loop calculates lps[i] for i = 1 to M-1
-                   while (i < M) {
-                       if (pat.charAt(i) == pat.charAt(len)) {
-                           len++;
-                           lps[i] = len;
-                           i++;
-                       }
-                       else // (pat[i] != pat[len])
-                       {
-                           // This is tricky. Consider the example.
-                           // AAACAAAA and i = 7. The idea is similar
-                           // to search step.
-                           if (len != 0) {
-                               len = lps[len - 1];
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
 
-                               // Also, note that we do not increment
-                               // i here
-                           }
-                           else // if (len == 0)
-                           {
-                               lps[i] = len;
-                               i++;
-                           }
-                       }
-                   }
-               }
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
     }
+}
 """
 
 component = Component(
@@ -187,7 +182,7 @@ async def join(session, details):
     except Exception as e:
         print("call error: {0}".format(e))
         # if the RPC is not registered in the server, publish the source code
-        session.publish("uk.ac.standrews.cs.mamoc.offloading", "Android", rpcname, code, "large", ["hi"])
+        session.publish("uk.ac.standrews.cs.mamoc.offloading", "Android", rpcname, class_code, "large", ["hi"])
 
     # async def on_event(result, duration):
     #     await self.sub.unsubscribe()
